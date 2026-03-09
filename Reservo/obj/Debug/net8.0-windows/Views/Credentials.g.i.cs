ffa607cc94c2b0707993cd1613423cb359874735
SaltyBooks <renehrg@mail.de>
2026-03-09 10:48:02 +0100

    Updated Visual Studio

--- Reservo/obj/Debug/net8.0-windows/Views/Credentials.g.i.cs
+++ Reservo/obj/Debug/net8.0-windows/Views/Credentials.g.i.cs
@@ -48,13 +48,13 @@ namespace Reservo.Views {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "9.0.12.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "9.0.13.0")]
         public void InitializeComponent() {
             if (_contentLoaded) {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/Reservo;component/views/credentials.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Reservo;V1.0.0.0;component/views/credentials.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\Views\Credentials.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
@@ -64,7 +64,7 @@ namespace Reservo.Views {
         }
         
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "9.0.12.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "9.0.13.0")]
         [System.ComponentModel.EditorBrowsableAttribute(System.ComponentModel.EditorBrowsableState.Never)]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Design", "CA1033:InterfaceMethodsShouldBeCallableByChildTypes")]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Maintainability", "CA1502:AvoidExcessiveComplexity")]
