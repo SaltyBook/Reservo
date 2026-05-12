b9b3ad1b6ac84886ee94c8793156c2d7fcad9723
SaltyBooks <renehrg@mail.de>
2026-05-12 19:38:34 +0200

    added table of employee hours

--- Reservo/ViewModels/StatisticViewModel.cs
+++ Reservo/ViewModels/StatisticViewModel.cs
@@ -1,12 +1,19 @@
 ﻿using PublicHoliday;
+using Reservo.Commands;
 using Reservo.Models;
 using Serilog;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.IO;
+using System.Text.Json;
+using System.Windows.Input;
 
 namespace Reservo.ViewModels
 {
     class StatisticViewModel : BaseViewModel
     {
+        private readonly string _employeeHoursPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "employeehours.json");
+
         private ObservableCollection<StatisticData> _statisticData;
         public ObservableCollection<StatisticData> StatisticData
         {
@@ -39,6 +46,24 @@ namespace Reservo.ViewModels
             }
         }
 
+        public ObservableCollection<EmployeeHours> Employees { get; set; } = new ObservableCollection<EmployeeHours>();
+
+        public EmployeeHoursSummary HoursSummary { get; set; } = new EmployeeHoursSummary();
+
+        private EmployeeHours _selectedEmployee;
+        public EmployeeHours SelectedEmployee
+        {
+            get => _selectedEmployee;
+            set
+            {
+                _selectedEmployee = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ICommand AddEmployeeCommand { get; }
+        public ICommand RemoveEmployeeCommand { get; }
+
         public StatisticViewModel()
         {
             Log.Information("StatisticViewModel initialisiert");
@@ -49,6 +74,13 @@ namespace Reservo.ViewModels
 
             var calendar = new GermanPublicHoliday { State = GermanPublicHoliday.States.HE };
             PublicHolidays = calendar.PublicHolidayNames(DateTime.Now.Year);
+
+            Employees.CollectionChanged += Employees_CollectionChanged;
+
+            AddEmployeeCommand = new RelayCommand(AddEmployee, null);
+            RemoveEmployeeCommand = new RelayCommand(RemoveEmployee, null);
+
+            LoadEmployeeHours();
         }
 
         public void FillStatisticData(ObservableCollection<WorkbookViewModel> workbooks)
@@ -85,6 +117,113 @@ namespace Reservo.ViewModels
             SelectedStatisticData = StatisticData.First(x => x.DisplayName.EndsWith(DateTime.Now.Year.ToString()));
 
             Log.Information("Laden für Statistiken abgeschlossen.");
+        }
+
+        public void AddEmployee(object? obj)
+        {
+            Employees.Add(new EmployeeHours()
+            {
+                Name = "Neuer Mitarbeiter"
+            });
+        }
+
+        public void RemoveEmployee(object? obj)
+        {
+            if (SelectedEmployee != null)
+                Employees.Remove(SelectedEmployee);
+        }
+
+        private void Employees_CollectionChanged(object sender, System.Collections.Specialized.NotifyCollectionChangedEventArgs e)
+        {
+            if (e.NewItems != null)
+            {
+                foreach (EmployeeHours employee in e.NewItems)
+                {
+                    employee.PropertyChanged += Employee_PropertyChanged;
+                }
+            }
+
+            if (e.OldItems != null)
+            {
+                foreach (EmployeeHours employee in e.OldItems)
+                {
+                    employee.PropertyChanged -= Employee_PropertyChanged;
+                }
+            }
+
+            RecalculateSummary();
+            SaveEmployeeHours();
+        }
+
+        private void Employee_PropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            RecalculateSummary();
+            SaveEmployeeHours();
+        }
+
+        private void RecalculateSummary()
+        {
+            HoursSummary.January = Employees.Sum(x => x.January);
+            HoursSummary.February = Employees.Sum(x => x.February);
+            HoursSummary.March = Employees.Sum(x => x.March);
+            HoursSummary.April = Employees.Sum(x => x.April);
+            HoursSummary.May = Employees.Sum(x => x.May);
+            HoursSummary.June = Employees.Sum(x => x.June);
+            HoursSummary.July = Employees.Sum(x => x.July);
+            HoursSummary.August = Employees.Sum(x => x.August);
+            HoursSummary.September = Employees.Sum(x => x.September);
+            HoursSummary.October = Employees.Sum(x => x.October);
+            HoursSummary.November = Employees.Sum(x => x.November);
+            HoursSummary.December = Employees.Sum(x => x.December);
+
+            HoursSummary.Total =
+                HoursSummary.January +
+                HoursSummary.February +
+                HoursSummary.March +
+                HoursSummary.April +
+                HoursSummary.May +
+                HoursSummary.June +
+                HoursSummary.July +
+                HoursSummary.August +
+                HoursSummary.September +
+                HoursSummary.October +
+                HoursSummary.November +
+                HoursSummary.December;
+
+            OnPropertyChanged(nameof(HoursSummary));
+        }
+
+        private void SaveEmployeeHours()
+        {
+            var json = JsonSerializer.Serialize(Employees,
+                new JsonSerializerOptions()
+                {
+                    WriteIndented = true
+                });
+
+            File.WriteAllText(_employeeHoursPath, json);
+        }
+
+        private void LoadEmployeeHours()
+        {
+            if (!File.Exists(_employeeHoursPath))
+                return;
+
+            var json = File.ReadAllText(_employeeHoursPath);
+
+            var employees = JsonSerializer.Deserialize<ObservableCollection<EmployeeHours>>(json);
+
+            if (employees == null)
+                return;
+
+            Employees.Clear();
+
+            foreach (var employee in employees)
+            {
+                Employees.Add(employee);
+            }
+
+            RecalculateSummary();
         }
     }
 }
